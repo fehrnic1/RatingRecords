af34826e274323a0c9790d357b24c370883b0459
Nicolas <fehrnic1@students.zhaw.ch>
2025-06-01 10:32:05 +0200

    d

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,6 @@
 import { MongoClient, ObjectId } from "mongodb"; // See https://www.mongodb.com/docs/drivers/node/current/quick-start/
 import { DB_URI } from "$env/static/private";
-
 const client = new MongoClient(DB_URI);
-
 await client.connect();
 const db = client.db("RatingRecords"); // select database
 
